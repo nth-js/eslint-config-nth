8b619c06aea8f35026a1226209cfc7aa1ff09e5a
Hung Nguyen <nthung2112@gmail.com>
2021-07-04 00:16:42 +0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  plugins: ['html', 'react', 'react-hooks', 'prettier'],
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:react-hooks/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'latest',
+    },
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  ignorePatterns: ['node_modules/'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+        printWidth: 120,
+        endOfLine: 'auto',
+      },
+    ],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'import/prefer-default-export': 'off',
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'no-await-in-loop': 'off',
+    'no-unused-vars': [
+      1,
+      {
+        ignoreRestSiblings: true,
+        argsIgnorePattern: 'res|next|^err|^ignore|^_',
+        caughtErrors: 'none',
+      },
+    ],
+  },
+};
---
+++ README.md
@@ -0,0 +1,53 @@
+# eslint-config-nth
+
+ESLint and Prettier configuration for NTH projects
+
+## Installation
+
+```sh
+npm install --save-dev eslint-config-nth
+```
+
+## Usage
+
+Then add the extends to your .eslintrc.js file:
+
+The ESLint rules go directly under "rules" while prettier options go under "prettier/prettier". Note that prettier rules overwrite anything in my config (trailing comma, and single quote), so you'll need to include those as well.
+
+```js
+module.exports = {
+  extends: 'nth',
+  rules: {
+    'no-debugger': 'off',
+    'no-await-in-loop': 'warning',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 120,
+        trailingComma: 'es5',
+        singleQuote: true,
+      },
+    ],
+  },
+};
+```
+
+## ESlint and Prettier in VS Code
+
+Update your VS Code settings to run the linter action on saving the file.
+
+```json
+{
+  "editor.formatOnSave": true,
+  "editor.defaultFormatter": "esbenp.prettier-vscode",
+  "[javascript]": {
+    "editor.formatOnSave": false
+  },
+  "editor.codeActionsOnSave": {
+    "source.fixAll.eslint": true,
+    "source.organizeImports": true
+  },
+  "files.autoSave": "onFocusChange",
+  "eslint.validate": ["javascript", "javascriptreact"]
+}
+```
